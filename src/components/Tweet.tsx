58ed8a37f1303196ac4029e0e248c4ad8b35f2e8
nohsangwoo <nsgr12@naver.com>
2021-09-05 21:33:09 +0900

    promise handling

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { deleteDoc, getFirestore, doc, updateDoc } from 'firebase/firestore';
 type Props = {
   tweetObj: any;
   isOwner: boolean;
 };
 const Tweet = ({ tweetObj, isOwner }: Props) => {
+  const [error, setError] = useState<Error>();
   const onDelete = async (event: React.MouseEvent<HTMLButtonElement>) => {
     const ok = window.confirm('삭제 하시겠습니까?');
     if (ok) {
       // implement delete function
       //   console.log('tweetObj', tweetObj.id);
       //   doc("컬렉션이름", "문서이름")
-      await deleteDoc(doc(getFirestore(), 'tweets', tweetObj.id));
+      await deleteDoc(doc(getFirestore(), 'tweets', tweetObj.id))
+        .then(() => console.log('Delete succeeded!'))
+        .catch(error => {
+          setError(error.message);
+        })
+        .finally(() => console.log('finally'));
     }
   };
   return (
