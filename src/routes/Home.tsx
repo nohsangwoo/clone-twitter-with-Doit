f168ac2cf20612de2b4ba002b093561c471e13e6
nohsangwoo <nsgr12@naver.com>
2021-09-05 13:44:23 +0900

    setting input form

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [tweet, setTweet] = useState('');
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    const {
+      currentTarget: { value },
+    } = event;
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        value={tweet}
+        onChange={onChange}
+        type="text"
+        placeholder="What's on your mind?"
+        maxLength={120}
+      />
+      <input type="submit" value="tweet" />
+    </form>
+  );
 };
 
 export default Home;
