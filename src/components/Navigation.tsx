68b82ae6e5ab3c1e6cc3100cbe9479d07613bab3
nohsangwoo <nsgr12@naver.com>
2021-09-26 22:55:29 +0900

    top nav item css

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,23 +2,38 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "store/store";
+import styled from "styled-components";
+
+const NavigationContainer = styled.nav`
+  display: flex;
+  justify-content: center;
+  padding: 40px 0;
+`;
+
+const NavItemWrapper = styled.div`
+  margin: 5px;
+  padding: 5px;
+  border-radius: 5px;
+  text-decoration: none;
+  box-shadow: 0 1px 4px rgb(0 0 0 / 55%);
+  transition: all 0.5s;
+  color: black;
+`;
 
 interface Props {}
 const Navigation = (props: Props) => {
   const userInfo = useSelector((state: RootState) => state.users.userInfo);
 
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">All tweets</Link>
-        </li>
+    <NavigationContainer>
+      <NavItemWrapper>
+        <Link to="/">Home</Link>
+      </NavItemWrapper>
 
-        <li>
-          <Link to="/profile">{`${userInfo.displayName}의 Profile`}</Link>
-        </li>
-      </ul>
-    </nav>
+      <NavItemWrapper>
+        <Link to="/profile">{`${userInfo.displayName}'s Profile`}</Link>
+      </NavItemWrapper>
+    </NavigationContainer>
   );
 };
 
